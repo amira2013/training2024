5443b7569833b6b08ab2b6dd8ac9bbd495df4d7a
STUDENTLOANER <drisschima42@gmail.com>
2024-11-21 20:05:24 -0500

    first commit

---
+++ src/main/java/Constructor.java
@@ -0,0 +1,17 @@
+public class Constructor {
+    int a;
+    double b;
+    String st;
+    char c;
+
+    public Constructor(int u,double b,String st){
+        this.a=u;
+        System.out.println(a);
+        System.out.println(u);
+        System.out.println(this.st);
+    }
+
+    public static void main(String[] args) {
+        Constructor con=new Constructor(9,8.9,"Sami");
+    }
+}
---
+++ src/main/java/JavaPractice01.java
@@ -0,0 +1,15 @@
+public class JavaPractice01 {
+    public static void main(String[] args) {
+        JavaPractice01 jbr=new JavaPractice01();
+        jbr.add(8,9);
+    }
+    public  void add(int a,int b){
+      int total=a+b;
+        System.out.println(total);
+        substr(8,99);
+    }
+    public  void substr(int c,int d){
+        int subs=c-d;
+        System.out.println(subs);
+    }
+}
